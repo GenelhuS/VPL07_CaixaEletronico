0c046f0b5325499fadb15d089f75387ecfb9b589
MrDotDawn <mrdawn992@gmail.com>
2022-10-26 22:53:13 -0300

    Começo do planejamento da função intruso 26/10 Signed-off-by: MrDotDawn <mrdawn992@gmail.com>

--- intruso.cpp
+++ intruso.cpp
@@ -1,11 +1,27 @@
 #include "intruso.hpp"
+#include <vector>
 
 Intruso::Intruso(){
 
 }
 
-void Intruso::set_senha_vazada(std::string){
-    
+void Intruso::set_senha_vazada(std::string vazou){
+    //ideias teóricas
+    senhacompleta = vazou;
+}
+
+std::string Intruiso::organizador(std::string senhacompleta){
+    std::vector <std::string> junta;
+    //ideias teóricas
+    for (int i = 0; i < 10; i++){
+        if (i == 0){
+            junta[i] = senhacompleta[i] + senhacompleta[i+1] + senhacompleta[15-i];
+            i++;
+        }else{
+            junta[i-1] = senhacompleta[i] + senhacompleta[i+1] + senhacompleta[15-i];
+            i++;
+        }
+    }   
 }
 
 std::string Intruso::crack_senha(){
--- intruso.hpp
+++ intruso.hpp
@@ -1,9 +1,18 @@
 #include <string>
+#include <iterator>
+#include <map>
 
 class Intruso{
+
+    private:
+        std::string senhacompleta;
+        std::map<std::string, std::string>;
+        std::string senhacrackeada;
+        
     /*Continue a implementação da classe Intruso*/
 
     public:
         void set_senha_vazada(std::string vazou);
+        std::string organizador(std::string senhacompleta);
         std::string crack_senha();
 };
